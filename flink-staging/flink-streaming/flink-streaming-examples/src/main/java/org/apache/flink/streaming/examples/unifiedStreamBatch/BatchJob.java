a2b55270981a898fd15647cd392dd729da623c36
fobeligi <faybeligianni@gmail.com>
2015-04-15 11:38:11 +0200

    Minor changes to configure file paths

--- flink-staging/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/unifiedStreamBatch/BatchJob.java
+++ flink-staging/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/unifiedStreamBatch/BatchJob.java
@@ -30,7 +30,7 @@ public class BatchJob implements Runnable, Serializable {
 
 	public BatchJob(ExecutionEnvironment execEnv) {
 		this.plan = execEnv.createProgramPlan();
-		this.executor = new LocalExecutor(false);
+		this.executor = new LocalExecutor();
 		executor.setTaskManagerNumSlots(4);
 		try {
 			executor.start();
