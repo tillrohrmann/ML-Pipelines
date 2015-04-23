2f5b2973f7ab3f91a6c473e8a4cd9702218af011
fobeligi <faybeligianni@gmail.com>
2015-04-23 19:18:01 +0300

    More changes to VFDT

--- flink-staging/flink-streaming/flink-streaming-examples/src/main/scala/org/apache/flink/streaming/scala/examples/incrementalML/classifier/VeryFastDecisionTree.scala
+++ flink-staging/flink-streaming/flink-streaming-examples/src/main/scala/org/apache/flink/streaming/scala/examples/incrementalML/classifier/VeryFastDecisionTree.scala
@@ -17,10 +17,61 @@
  */
 package org.apache.flink.streaming.scala.examples.incrementalML.classifier
 
-import org.apache.flink.ml.common.WithParameters
+import org.apache.flink.api.common.functions.FlatMapFunction
+import org.apache.flink.api.scala.functions.FlatMapFunction
+import org.apache.flink.api.scala._
+import org.apache.flink.ml.common.{LabeledVector, Parameter, WithParameters}
+import org.apache.flink.ml.math.DenseVector
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import VeryFastDecisionTree._
+import org.apache.flink.util.Collector
 
-class VeryFastDecisionTree
-  extends WithParameters{
+class VeryFastDecisionTree(context: StreamExecutionEnvironment)
+  extends Serializable
+  with WithParameters {
 
+  def setMinNumberOfInstances(minInstances: Int): VeryFastDecisionTree = {
+    parameters.add(minNumberOfInstances, minInstances)
+    this
+  }
 
+  def setThreshold(thresh: Double): VeryFastDecisionTree = {
+    parameters.add(threshold, thresh)
+    this
+  }
+
+  val dataPointsStream = context.fromCollection(data).map(dp => DataPoints(dp))
+  dataPointsStream.
+}
+
+object VeryFastDecisionTree {
+
+  /** Minimum number of instances seen, before deciding the new splitting feature.
+    *
+    */
+  case object minNumberOfInstances extends Parameter[Int] {
+    override val defaultValue: Option[Int] = Some(200)
+  }
+
+  /** Hoeffding Bound threshold
+    *
+    */
+  case object threshold extends Parameter[Double] {
+    override val defaultValue: Option[Double] = Some(1.0)
+  }
+
+  def apply(context: StreamExecutionEnvironment): VeryFastDecisionTree = {
+    new VeryFastDecisionTree(context)
+  }
+
+  val data: Seq[LabeledVector] = List(LabeledVector(1.0, DenseVector(Array(2104.00, 3.00))),
+    LabeledVector(1.0, DenseVector(Array(1600.00, 3.00))),
+    LabeledVector(1.0, DenseVector(Array(2400.00, 3.00)))
+  )
+
+  def main(args: Array[String]) {
+    val con = StreamExecutionEnvironment.getExecutionEnvironment
+    VeryFastDecisionTree(con)
+    con.execute()
+  }
 }
