ec322b7fea8e12662bb9c6940c0154c9847bc074
marthavk <martha.1@hotmail.com>
2015-05-19 17:12:53 +0200

    implemented GreedySampler.java

--- flink-staging/flink-streaming/flink-streaming-ml/src/main/java/org/apache/flink/streaming/sampling/examples/GreedySamplingExample.java
+++ flink-staging/flink-streaming/flink-streaming-ml/src/main/java/org/apache/flink/streaming/sampling/examples/GreedySamplingExample.java
@@ -22,6 +22,7 @@ import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.sampling.evaluators.DistanceEvaluator;
 import org.apache.flink.streaming.sampling.evaluators.DistributionComparator;
 import org.apache.flink.streaming.sampling.generators.DataGenerator;
 import org.apache.flink.streaming.sampling.generators.GaussianDistribution;
