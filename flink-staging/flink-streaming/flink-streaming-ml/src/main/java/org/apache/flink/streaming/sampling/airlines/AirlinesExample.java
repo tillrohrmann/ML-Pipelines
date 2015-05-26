6f2b0879bfb236ee10a0c9873f01bcefacb76ed3
marthavk <martha.1@hotmail.com>
2015-05-26 14:40:47 +0200

    changes in AirlinesExample, calculate queries in the whole, not sampled stream

--- flink-staging/flink-streaming/flink-streaming-ml/src/main/java/org/apache/flink/streaming/sampling/airlines/AirlinesExample.java
+++ flink-staging/flink-streaming/flink-streaming-ml/src/main/java/org/apache/flink/streaming/sampling/airlines/AirlinesExample.java
@@ -16,13 +16,27 @@
  * limitations under the License.
  */
 package org.apache.flink.streaming.sampling.airlines;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.commons.net.ntp.TimeStamp;
+import org.apache.flink.api.common.functions.*;
+import org.apache.flink.api.java.operators.translation.PlanFilterOperator;
+import org.apache.flink.api.java.tuple.*;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.streaming.runtime.tasks.StreamingRuntimeContext;
+import org.apache.flink.streaming.sampling.helpers.MetaAppender;
 import org.apache.flink.streaming.sampling.helpers.SamplingUtils;
+import org.apache.flink.streaming.sampling.helpers.SimpleUnwrapper;
+import org.apache.flink.streaming.sampling.helpers.StreamTimestamp;
+import org.apache.flink.streaming.sampling.samplers.BiasedReservoirSampler;
+import org.apache.flink.streaming.sampling.samplers.ReservoirSampler;
+import org.apache.flink.streaming.sampling.samplers.Sample;
+import org.apache.flink.util.Collector;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Properties;
 
 
 /**
@@ -42,51 +56,119 @@ import org.apache.flink.streaming.sampling.helpers.SamplingUtils;
 public class AirlinesExample {
 
 	public static void main(String[] args) throws Exception {
+
 		String path = SamplingUtils.path;
+		Properties initProps = SamplingUtils.readProperties(SamplingUtils.path + "distributionconfig.properties");
+		int sample_size = Integer.parseInt(initProps.getProperty("sampleSize"));
 		/*set execution environment*/
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.setParallelism(1);
+		DataStreamSource<String> source = env.readTextFile(path + "january_dataset.data")	;
+		//DataStreamSource<String> source = env.readTextFile(path + "xs_dataset.data")	;
+		/*
+		 * Tuple8 fields:
+		 * f0 day of january (Integer)
+		 * f1 day of week (Integer)
+		 * f2 crs depart time (Integer)
+		 * f3 unique carrier (String)
+		 * f4 origin (String)
+		 * f5 destination (String)
+		 * f6 delay (Integer)
+		 * f7 1
+		 */
+		SingleOutputStreamOperator<Tuple8<Integer,Integer,Integer,String,String,String,Integer,Integer>,?> dataStream =
+				source.map(new MapFunction<String, Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer>>() {
+					@Override
+					public Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer> map(String record) throws Exception {
+						Tuple8 out = new Tuple8();
+						String[] values = record.split(",");
+						int[] integerFields = new int[]{1,2,3};
+						int[] stringFields = new int[]{4, 5, 6};
+						int[] integerFields2 = new int[]{7,8};
 
-		DataStreamSource<String> source = env.readTextFile(path + "2008_14col.data")	;
+						int counter = 0;
+						for(int i:integerFields) {
+							int curr = Integer.parseInt(values[i]);
+							out.setField(curr, counter);
+							counter ++;
+						}
 
-		SingleOutputStreamOperator<Tuple2<Integer[], String[]>, ?> dataStream =
-				source.map(new MapFunction<String, Tuple2<Integer[], String[]>>() {
+						for (int i:stringFields) {
+							out.setField(values[i],counter);
+							counter++;
+						}
 
-				@Override
-				public Tuple2<Integer[], String[]> map(String record) throws Exception {
+						for (int i:integerFields2) {
+							int curr = Integer.parseInt(values[i]);
+							out.setField(curr,counter);
+							counter++;
+						}
 
-					String[] values = record.split(",");
-					Integer[] vInt = new Integer[11];
-					int[] integerFields = new int[]{0, 1, 2, 3, 4, 5, 7, 8, 11, 12, 13};
 
-					for(int i=0; i<integerFields.length; i++) {
-						int index = integerFields[i];
-						vInt[i] = Integer.parseInt(values[index]);
+						return out;
+
 					}
+				});
 
-					String[] vStr = new String[3];
-					int[] stringFields = new int[]{6, 9, 10};
-					for (int i=0; i<stringFields.length; i++) {
-						int index = stringFields[i];
-						vStr[i] = (values[index]);
+		//HEAVY HITTERS
+		/*dataStream
+				.filter(new FilterFunction<Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer>>() {
+					@Override
+					public boolean filter(Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer> value) throws Exception {
+						return !value.f5.equals("ATL") && !value.f5.equals("ORD") && !value.f5.equals("DFW")
+								&& !value.f5.equals("DEN") && !value.f5.equals("LAX") && !value.f5.equals("PHX")
+								&& !value.f5.equals("LAS") && !value.f5.equals("IAH") && !value.f5.equals("DTW");
 					}
-
-					return new Tuple2<Integer[], String[]>(vInt, vStr);
-
-				}
-			});
-
-		dataStream.addSink(new RichSinkFunction<Tuple2<Integer[], String[]>>() {
+				})
+				.groupBy(5).sum(7).filter(new FilterFunction<Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer>>() {
 			@Override
-			public void invoke(Tuple2<Integer[], String[]> value) throws Exception {
-				System.out.println(value.toString());
+			public boolean filter(Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer> value) throws Exception {
+				return value.f7>12090;
 			}
-		});
+		})
+				.print();*/
+
+		//RANGE QUERIES
+		/*dataStream.filter(new FilterFunction<Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer>>() {
+			@Override
+			public boolean filter(Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer> value) throws Exception {
+				return value.f0<8;
+			}
+		}).filter(new FilterFunction<Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer>>() {
+			@Override
+			public boolean filter(Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer> value) throws Exception {
+				return value.f1==6||value.f1==7;
+			}
+		}).count().print();*/
+
+
+		//SAMPLE
+		dataStream.map(new ReservoirSampler<Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer>>(sample_size))
+				.flatMap(new FlatMapFunction<Sample<Tuple8<Integer,Integer,Integer,String,String,String,Integer,Integer>>, String>() {
+					StreamTimestamp s = new StreamTimestamp();
+					@Override
+					public void flatMap(Sample<Tuple8<Integer, Integer, Integer, String, String, String, Integer, Integer>> value, Collector<String> out) throws Exception {
+						StreamTimestamp now = new StreamTimestamp();
+						long time = now.getTimestamp()-s.getTimestamp();
+						if (time > 1000) {
+							//make string:
+							String str = new String();
+							ArrayList<Tuple8> allSamples = new ArrayList<Tuple8>();
+							for (Tuple8 sample: allSamples) {
+								String cTuple = sample.toString();
+								/*cTuple = cTuple.substring(0)*/
+
+							}
+						}
+					}
+				});
 
 		/*get js for execution plan*/
 		System.err.println(env.getExecutionPlan());
 
 		/*execute program*/
 		env.execute();
+
 	}
 
 
